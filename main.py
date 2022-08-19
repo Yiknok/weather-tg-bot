e94825597d8b075f2168d0d5693088eabcc7a313
Yiknok <danyapavl20.03@gmail.com>
2022-08-19 15:42:46 +0300

    deploy version without any automation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as b
 import telebot
-import schedule
 
 URL = 'https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B8%D0%B5%D0%B2'
 token = '5729867659:AAF8B244CiNMQNhd66is5NDYrcXoC5vkYjs'
@@ -12,7 +11,6 @@ def parser(url):
     soup = b(r.text, 'html.parser')
     temperature = soup.find_all('p', class_='today-temp')
     return [c.text for c in temperature]
-
 
 
 bot = telebot.TeleBot(token)
@@ -31,7 +29,4 @@ def weather_today(message):
 
 bot.polling()
 
-schedule.every().day.at('06:00').do(parser, URL)
-while True:
-    schedule.run_pending()
 
